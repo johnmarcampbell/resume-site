0fe4e330705398afb2078bb05ea7656d2ad296dd
John Campbell <johnmarcampbell@gmail.com>
2018-04-06 18:43:48 -0400

    Put resume info into separate settings file

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from .settings import default_data
 
 bootstrap = Bootstrap()
 
-def create_app():
+def create_app(data=default_data):
     """Function to create and return an application"""
     app = Flask(__name__)
+    app.config['data'] = data
     bootstrap.init_app(app)
 
     from .main import main as blueprint
