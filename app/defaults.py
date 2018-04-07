7598a9e15d325237898ffccdca9f71149d7402a8
John Campbell <johnmarcampbell@gmail.com>
2018-04-07 12:39:16 -0400

    Add some comments to defaults.py

--- app/defaults.py
+++ app/defaults.py
@@ -1,3 +1,8 @@
+"""
+Summary
+-------
+The summary is a brief intro. You can put raw HTML into this field.
+"""
 summary = '<p>Summarise your career here lorem ipsum dolor sit amet, consectetuer adipiscing elit. You can <a href="http://themes.3rdwavemedia.com/website-templates/orbit-free-resume-cv-template-for-developers/" target="_blank">download this free resume/CV template here</a>. Aenean commodo ligula eget dolor aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.</p>'
 
 
@@ -23,6 +28,16 @@ skills = [
         ['Sketch & Photoshop', '60%']
         ]
 
+"""
+Experience
+----------
+This should be a list of lists. Each sublist corresponds to a particular job
+and is of the form:
+    ['Title', 'Date range', 'Company name and location', 'Description of role']
+
+The 'Description of role' field does not get escaped by the templating engine,
+so you can put raw HTML in it if you like.
+"""
 experience = [
         ['Lead Developer',
             '2015 - Present',
@@ -41,6 +56,14 @@ experience = [
         ]
     ]
 
+"""
+Projects
+--------
+The project_intro field is for a short introduction to your work.
+Project are a list of lists, where each sublist refers to a specific project,
+and is of the form:
+    ['Title', 'Description', 'Link to project webpage']
+"""
 project_intro = '<p>You can list your side projects or open source libraries in this section. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et ligula in nunc bibendum fringilla a eu lectus.</p>'
 projects = [
         ['Velocity',
@@ -67,6 +90,12 @@ projects = [
 
 
 
+"""
+default_data
+------------
+This dictionary puts everything together. It will be read by the Flask app when
+it is instantiated.
+"""
 default_data = {
     'site_title' : 'Responsive Resume/CV Template for DevelopersResponsive Resume/CV Template for Developers',
     'name' : 'Alan Doe',
